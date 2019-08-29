0fa30d972cddf21bdfd2901f69920819b09d0f93
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-08-29 15:56:21 +0200

    First commit

---
+++ src/main/java/com/ab/springreactoroperations/SpringReactorOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.ab.springreactoroperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ab/springreactoroperations/SpringReactorOperationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ab.springreactoroperations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReactorOperationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
