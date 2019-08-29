e6a059b3241c607e59cd28185d5597092753183e
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-08-29 22:53:15 +0200

    Adds unit tests

--- src/test/java/com/ab/springreactoroperations/CreationOperationTest.java
+++ src/test/java/com/ab/springreactoroperations/CreationOperationTest.java
@@ -5,6 +5,7 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,6 +49,43 @@ public class CreationOperationTest {
         .expectNext(3)
         .expectNext(4)
         .expectNext(5)
+        .verifyComplete();
+  }
+
+  @Test
+  public void createFluxInterval() {
+    Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1))
+        .take(5); //  Take operation defines the the limit of results.
+
+    StepVerifier.create(intervalFlux)
+        .expectNext(0L)
+        .expectNext(1L)
+        .expectNext(2L)
+        .expectNext(3L)
+        .expectNext(4L)
+        .verifyComplete();
+  }
+
+  @Test
+  public void mergeFluxes() {
+    Flux<String> characterFlux = Flux
+        .just("Garfield", "Kojak", "Barbossa")
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> foodFlux = Flux
+        .just("Lasagna", "Lollipops", "Apples")
+        .delaySubscription(Duration.ofMillis(250))
+        .delayElements(Duration.ofMillis(500));
+
+    Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+    StepVerifier.create(mergedFlux)
+        .expectNext("Garfield")
+        .expectNext("Lasagna")
+        .expectNext("Kojak")
+        .expectNext("Lollipops")
+        .expectNext("Barbossa")
+        .expectNext("Apples")
         .verifyComplete();
   }
 
